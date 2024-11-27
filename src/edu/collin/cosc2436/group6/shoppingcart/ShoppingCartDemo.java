c8bd3e50665274eb338e3249ceb1936eb24cfaba
Juanchivp12 <Juanvillegasse@gmail.com>
2024-11-26 21:01:35 -0600

    trying git

--- src/edu/collin/cosc2436/group6/shoppingcart/ShoppingCartDemo.java
+++ src/edu/collin/cosc2436/group6/shoppingcart/ShoppingCartDemo.java
@@ -185,7 +185,7 @@ public class ShoppingCartDemo
 	}
 	
 	/**
-	 * Private helper to add items and their price to each type
+	 * Private helper to add items and their price to each type.
 	 * @param items item to set the price for
 	 * @param lookUp
 	 * @throws MissingCategoryException exception to be thrown when there is a missing category
